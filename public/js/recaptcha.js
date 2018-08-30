0dab3aa07ee0a28d88aaba99b49f367d67b6bb5b
Junior <jsantos.class@gmail.com>
2018-08-30 10:35:49 -0300

    Anúncios sugeridos e link pra revenda

--- public/js/recaptcha.js
+++ public/js/recaptcha.js
@@ -1,3 +1,3 @@
 $(document).ready(function(){
-  $('form').html($('form').html()+'<div class="g-recaptcha" data-sitekey="6LfJEm0UAAAAAKCvlnrl_9VdbGI6CGbL4jNlMKSt"></div>');
+  //$('form').html($('form').html()+'<div class="g-recaptcha" data-sitekey="6LfJEm0UAAAAAKCvlnrl_9VdbGI6CGbL4jNlMKSt"></div>');
 });
