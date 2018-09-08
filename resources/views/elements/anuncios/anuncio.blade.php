62d5eacb94d0efe33c32691771c264b43556ff8f
Junior <jsantos.class@gmail.com>
2018-09-08 09:40:54 -0300

    Atualização

--- resources/views/elements/anuncios/anuncio.blade.php
+++ resources/views/elements/anuncios/anuncio.blade.php
@@ -1,6 +1,6 @@
 <div class="row">
   <div class="col-sm-12">
-    <a href="/anuncios/{{str_replace(" ", "-", $anuncio->getNomeFormated())}}_{{$anuncio->id}}" class="list-group-item list-group-item-action flex-column align-items-start mt-1 mb-1">
+    <a href='/anuncios/{{str_replace(" ", "-", $anuncio->getNomeFormated())}}_{{$anuncio->id}}' class="list-group-item list-group-item-action flex-column align-items-start mt-1 mb-1 {{!$anuncio->patrocinado?'patrocinado':''}}">
       <div class="row">
         <div class="col-sm-5">
           @php
@@ -21,7 +21,11 @@
             <h5 class="mb-1">{{$anuncio->nome}}</h5>
             <small>{{$anuncio->created_at->format('d/m/Y H:i')}}</small>
           </div>
-          <h5 class="mb-1"><span class="badge badge-success">R${{number_format(substr($anuncio->valor.'0', 0, -3), 2, ",", ".")}}</span></h5>
+          <h5 class="mb-1">
+            <span class="badge badge-success">
+              R${{number_format(substr($anuncio->valor.'0', 0, -3), 2, ",", ".")}}
+            </span>
+          </h5>
           <p class="mb-1">{{str_limit($anuncio->descricao, 200, '...')}}</p>
           <hr>
           <div class="row">
@@ -39,7 +43,18 @@
             </div>
           </div>
           <hr>
-          <small class="text-right">{{App\User::find($anuncio->user)->name}}</small>
+          <div class="row">
+            <small class="col-sm-6 text-left">
+            {{
+                $anuncio->users->isRevenda()?
+                $anuncio->users->isRevenda()->nomefantasia : $anuncio->users->name
+            }}
+            </small> 
+            <small class="col-sm-6 w-100 text-right" >
+              {!! !$anuncio->patrocinado?'<span class="badge badge-success">Anúncio em destaque</span>':'' !!}
+            </small>  
+          </div>
+          
         </div>
       </div>
     </a>
