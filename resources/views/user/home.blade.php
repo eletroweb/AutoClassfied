bc0b1f540859d43897744f33f569813435fbdada
dev <dev@proimagem3d.com.br>
2018-09-23 09:16:17 -0400

    Alterações no minha conta

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -5,11 +5,19 @@
     <h2>Olá {{Auth::user()->name}}, bem-vindo(a) ao Único dono!</h2>
     <hr>
   </div>
-  <div class="col-sm-12">
+  <div class="col-sm-6">
     <div class="card">
       <div class="card-body">
         <h5 class="card-title">Anúncios</h5>
-        <p class="card-text">{{Auth::user()->anuncios->count() > 0?Auth::user()->anuncios->count()." anúncios publicados":"Nenhum anúncio ativo"}}</p>
+        <p class="card-text">{{Auth::user()->anuncios->count() > 0?Auth::user()->anuncios->count()." anúncio(s) publicados":"Nenhum anúncio ativo"}}</p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Data de cadastro</h5>
+        <p class="card-text">{{Auth::user()->created_at->format('d/m/Y')}}</p>
       </div>
     </div>
   </div>
