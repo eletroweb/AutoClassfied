1ed3a63e2d102ff9e2ed7eff46ae7c42b1c50b18
Junior <jsantos.class@gmail.com>
2018-09-25 10:37:05 -0300

    Fix telefone

--- public/js/custom.js
+++ public/js/custom.js
@@ -163,7 +163,7 @@ $(document).ready(function(){
   $('.cpf').mask('000.000.000-00', {reverse: true});
   $('.cnpj').mask('00.000.000/0000-00', {reverse: true});
   $('.cep').mask('00000-000');
-  $('#telefone').mask('(00) 0000-0000');
+  $('#telefone').mask('(00) 00000-0000');
   $('#celular').mask('(00) 0 0000-0000');
   $('#valor').mask("#.##0,00", {reverse: true});
   $('.valor').mask("#.##0,00", {reverse: true});
