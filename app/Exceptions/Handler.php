4dfd8a378edf5107cfe8adc86297964491d3a142
dev <dev@proimagem3d.com.br>
2018-09-26 12:00:52 -0400

    Atualização

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -48,9 +48,9 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        if(env('APP_DEBUG') == 'true'){
-            return parent::render($request, $exception);    
-        }
-        return redirect(route('anuncios'));
+        //if(env('APP_DEBUG') == 'true'){
+        return parent::render($request, $exception);    
+        //}
+        //return redirect(route('anuncios'));
     }
 }
