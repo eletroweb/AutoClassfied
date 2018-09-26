ce1e816b808c3c290d16b4e96f27bdd708432a9b
Junior <jsantos.class@gmail.com>
2018-09-26 09:29:26 -0300

    Atualização redirecionamento

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -48,6 +48,9 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
+        if(env('APP_DEBUG') == 'true'){
+            return parent::render($request, $exception);    
+        }
+        return redirect(route('anuncios'));
     }
 }
