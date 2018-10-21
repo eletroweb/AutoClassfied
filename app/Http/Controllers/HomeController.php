b4ae54ec6271c16081714f1743a6f95ff58b1756
Junior <jsantos.class@gmail.com>
2018-10-21 10:24:18 -0300

    Atualização

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,7 +15,10 @@ class HomeController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index(){
-        $recentes = Anuncio::where('ativo', true)->orderBy('id', 'desc')->get()->random(10);
+        $recentes = Anuncio::where('ativo', true)->orderBy('id', 'desc')->get();
+        if(!$recentes->isEmpty()){
+            $recentes = $recentes->random(10);
+        }
         $marcas = Marca::all();
         return view('home')->with(['recentes' => $recentes, 'home'=>true, 'marcas'=> $marcas]);
     }
