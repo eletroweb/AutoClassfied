222bf776d91781e93cf1d39182623a65b59dba92
Junior Santos <jsantos.class@gmail.com>
2019-01-07 00:31:22 -0300

    Tela de manutenção

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<title>UNICODONO - Manutenção</title>
+<style>
+  body { text-align: center; padding: 150px; }
+  h1 { font-size: 50px; }
+  body { font: 20px Helvetica, sans-serif; color: #333; }
+  article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+  a { color: #dc8100; text-decoration: none; }
+  a:hover { color: #333; text-decoration: none; }
+</style>
+
+<article>
+    <h1>Estamos em manutenção</h1>
+    <div>
+        <p>
+          Pedimos desculpas pelo transtorno, mas estamos trabalhando para fazer o Únicodono melhor para você.
+          Voltaremos em alguns instantes.
+        </p>
+        <p>&mdash; Equipe Únicodono</p>
+    </div>
+</article>
